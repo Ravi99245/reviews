7d9b2e59bfce3d46d868d05b37e3663d4e261d88
Ravi <vasamsettiravi151@gmail.com>
2023-11-07 11:13:15 +0000

    secondcommit

--- src/components/ReviewsCarousel/index.js
+++ src/components/ReviewsCarousel/index.js
@@ -5,13 +5,13 @@ let list = ''
 let currentIndex = 0
 
 class ReviewsCarousel extends Component {
-  state = {currentReview: list[currentIndex]}
-
   constructor(props) {
     super(props)
     const {reviewsList} = this.props
     list = reviewsList
   }
+
+  state = {currentReview: list[currentIndex]}
 
   decrementIndex = () => {
     if (currentIndex > 0) {
@@ -31,17 +31,21 @@ class ReviewsCarousel extends Component {
   render() {
     const {currentReview} = this.state
     const {imgUrl, username, companyName, description} = currentReview
-    console.log(currentIndex)
+    console.log(currentReview)
     return (
       <div className="bg-container">
         <h1 className="heading">Reviews</h1>
         <div className="review-container">
-          <button className="button" type="button" data-testid="leftArrow">
+          <button
+            className="button"
+            type="button"
+            data-testid="leftArrow"
+            onClick={this.decrementIndex}
+          >
             <img
               src="https://assets.ccbp.in/frontend/react-js/left-arrow-img.png"
               alt="left arrow"
               className="arrow"
-              onClick={this.decrementIndex}
             />
           </button>
           <div className="profile-container">
@@ -50,12 +54,16 @@ class ReviewsCarousel extends Component {
             <p className="company">{companyName}</p>
             <p className="review">{description}</p>
           </div>
-          <button type="button" className="button" data-testid="rightArrow">
+          <button
+            type="button"
+            className="button"
+            data-testid="rightArrow"
+            onClick={this.incrementIndex}
+          >
             <img
               src="https://assets.ccbp.in/frontend/react-js/right-arrow-img.png"
               alt="right arrow"
               className="arrow"
-              onClick={this.incrementIndex}
             />
           </button>
         </div>
